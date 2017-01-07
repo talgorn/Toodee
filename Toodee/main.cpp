2878646d740132266e3adb40e431bf4a3b199ec4
François-Xavier Talgorn <github@talgorn.me>
2017-01-07 12:02:00 +0100

    Initial Commit

---
+++ Toodee/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Toodee
+//
+//  Created by Talgorn François-Xavier on 07/01/2017.
+//  Copyright © 2017 Talgorn François-Xavier. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
